6d2180c792196b4884fd8c4c7375d2a3d61c97f1
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-10-17 13:46:40 +0800

    create table schema migration

---
+++ database/migrations/2024_10_17_050419_create_tiers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tiers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('photo');
+            $table->string('tagline');
+            $table->unsignedBigInteger('quality');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('duration');
+            $table->foreignId('package_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tiers');
+    }
+};
